5611f17f32bf8810479da4307c6e0cd4662f0893
Nathan Rini <nate@schedmd.com>
2025-01-22 11:10:15 -0700

    Improve test-build.sh
    Wait for slurmctld to startup and then run a simple job.
    
    Ticket: 28148
    Issue: 11

--- scaleout/test-build.sh
+++ scaleout/test-build.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-scontrol ping
+MAX_TRY=150
+
+# Wait for slurmctld to startup fully
+for ((try = 0; try < $MAX_TRY; try++))
+do
+	status="$(scontrol ping --json | jq -r '.pings[0].responding')"
+	rc=$?
+
+	if [ $? -eq 0 -a "${status}" = "true" ]
+	then
+		break
+	elif [ $try -ge $MAX_TRY ]
+	then
+		echo "FAILED"
+		exit 1
+	fi
+
+	sleep $(echo "scale=5; 0.1 * $try" | bc)
+done
+
+exec srun -N10 uptime
