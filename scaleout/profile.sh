a4907c71cdbebb70e5008ac29c54cbd7cd192315
Nathan Rini <nate@schedmd.com>
2025-02-14 12:23:15 -0700

    Add /usr/local/{bin,sbin} to PATH
    Issue: 14

--- scaleout/profile.sh
+++ scaleout/profile.sh
@@ -3,3 +3,4 @@ export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/
 export LD_LIBRARY_PATH=/usr/local/lib64:/usr/local/lib
+export PATH=/home/$USER/.local/bin:/home/$USER/bin:/usr/local/bin/:/usr/local/sbin/:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin
