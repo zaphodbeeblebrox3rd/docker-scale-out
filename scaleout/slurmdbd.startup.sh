f6417edcf137fc761c9b890a7c619034a2a98b68
Nathan Rini <nate@schedmd.com>
2025-03-19 13:41:19 -0600

    Create /var/run/slurmdbd
    Needed for sackd.socket.
    
    Issue: 17

--- scaleout/slurmdbd.startup.sh
+++ scaleout/slurmdbd.startup.sh
@@ -5,6 +5,9 @@
 touch /var/log/slurmdbd.log
 chown slurm:slurm /var/log/slurmdbd.log
 
+mkdir -p /var/run/slurmdbd/
+chown slurm:slurm /var/run/slurmdbd/
+
 date > /var/run/slurmdbd.startup
 
 exit 0
